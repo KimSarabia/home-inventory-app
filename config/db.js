f0f1f47ce97c2dda6d426c0b646b725c866f979a
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-27 09:34:45 -0700

    ready for heroku

--- config/db.js
+++ config/db.js
@@ -9,6 +9,16 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL || {
     database: "home_inventory"
 });
 
+db.query(`CREATE TABLE IF NOT EXISTS items (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        item_name VARCHAR(100),
+        item_value DECIMAL(10, 2),
+        item_room VARCHAR(100)
+      )`);
+      
+db.query(`CREATE TABLE IF NOT EXISTS rooms (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        room_type VARCHAR(255)
+      )`);
+
 connection.connect(function(err){
 if(err){
   console.log('Error:', err);
@@ -17,5 +27,6 @@ else{
   console.log('Connection success!');
 }
 });
+
 
 module.exports = connection;
